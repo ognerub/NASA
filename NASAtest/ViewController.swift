3978e0ae18f27133a51997566d114cd4b31b4b33
ognerub <a.ulianovskiy@yandex.ru>
2024-02-09 11:05:56 +0300

    Initial Commit

---
+++ NASAtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NASAtest
+//
+//  Created by Admin on 2/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
